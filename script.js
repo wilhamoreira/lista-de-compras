2de7d0547d5c7f47b4ef93947050504c84bffbeb
wilhamoreira <wilhadev@gmail.com>
2023-09-23 11:49:17 -0300

    Ajustando tamanho da tabela

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const adicao = document.querySelector('button#adicionar-produto')
 
 function adicionaProduto(){
-    const table = document.querySelector('table#lista')
-    table.innerHTML += `<tr class="produto">
-        <td><input type="text"  name="nome-produto" class="nome-produto"></td>
-        <td><input type="number"  name="valor-unitario" class="valor-unitario" ></td>
-        <td><input type="number"  name="quantidade" class="quantidade" ></td>
-        <td><input type="number"  name="sub-total" class="sub-total" ></td>
+    let table = document.querySelector('.lista')
+    //criando novo html
+    const newHTML = `<tr class="produto">
+    <td><input type="text"  name="nome-produto" class="nome-produto"></td>        
+    <td><input type="number"  name="valor-unitario" class="valor-unitario"></td>
+    <td><input type="number"  name="quantidade" class="quantidade"></td>
+    <td><input type="number"  name="sub-total" class="sub-total"></td>
     </tr>`
+    //inserindo o novo hmtl sem perder os dados preenchidos no formulario
+    table.insertAdjacentHTML("beforeend", newHTML)
+
 
 
     
